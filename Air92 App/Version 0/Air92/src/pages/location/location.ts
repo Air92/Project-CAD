ae32cca751f510e94999362e41f2e4846f5b187c
YusofBandar <ybandar1902@gmail.com>
2018-01-17 19:37:10 +0000

    add journey

--- Air92 App/Version 0/Air92/src/pages/location/location.ts
+++ Air92 App/Version 0/Air92/src/pages/location/location.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 
+
 /**
  * Generated class for the LocationPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+
+
+ declare var google;
+
+
 
 //@IonicPage()
 @Component({
@@ -16,6 +22,8 @@ import { Geolocation } from '@ionic-native/geolocation';
 })
 export class LocationPage {
 location : any;
+placeSearch : any;
+startSearch : any;
 
 constructor(public navCtrl: NavController, private geolocation: Geolocation) {
   }
@@ -31,6 +39,35 @@ constructor(public navCtrl: NavController, private geolocation: Geolocation) {
     });
 
   }
+
+  placesSearchInput : any;
+  ngAfterViewInit() {
+      this.placesSearchInput =  document.getElementById("startSearch").getElementsByTagName('input')[0];
+      
+    }
+  
+    initAutoComplete(){
+      console.log("hit");
+      console.log(this.placesSearchInput);
+      this.startSearch = new google.maps.places.Autocomplete((this.placesSearchInput));
+  
+  }
+
+ 
+ 
+
+   // This example displays an address form, using the autocomplete feature
+      // of the Google Places API to help users fill in the information.
+
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+       
+
+
+
+      
 
 
 
