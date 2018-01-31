50acafd13e3720e5aeb611711b3b5ed8ce6c2925
unknown <ybandar1902@gmail.com>
2018-01-31 11:28:14 +0000

    XML Request
    Signed-off-by: unknown <ybandar1902@gmail.com>

--- Air92 App/Version 0/Air92/src/pages/location/location.ts
+++ Air92 App/Version 0/Air92/src/pages/location/location.ts
@@ -141,26 +141,14 @@ export class LocationPage
     return new Promise((resolve, reject) => {
       var xhr = new XMLHttpRequest();
       xhr.open('GET', url);
-      xhr.onload = function () {
-        if (this.status >= 200 && this.status < 300) {
+      xhr.onload = function (status) {
+      {
           var result = JSON.parse(xhr.response);
           resolve(result.results[0].geometry.location);
-          
-        } else {
-          reject({
-            status: this.status,
-            statusText: xhr.statusText
-          });
-        }
-      };
-      xhr.onerror = function () {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText
-        });
-      };
+      } 
+    };
       xhr.send();
-    });
+  });
 
   
   
--- Air92 App/Version 0/Air92/src/pages/login/login.ts
+++ Air92 App/Version 0/Air92/src/pages/login/login.ts
@@ -48,21 +48,8 @@ export class LoginPage {
       var xhr = new XMLHttpRequest();
       xhr.open('GET', url);
       xhr.onload = function () {
-        if (this.status >= 200 && this.status < 300) {
           console.log(xhr.response);
           resolve(xhr.response);
-        } else {
-          reject({
-            status: this.status,
-            statusText: xhr.statusText
-          });
-        }
-      };
-      xhr.onerror = function () {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText
-        });
       };
       xhr.send();
     });
